e24914cd781b0540664b19b276eecf6e8a8d1eaf
Roman Kozko <romanka2001@mail.ru>
2021-11-30 21:41:30 +0300

    add model of container

---
+++ DILab/DIContainerLib/Container.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainerLib
+{
+    public struct Container
+    {
+        public Type type { get; }
+        public Lifetime lifetime { get; }
+
+        public Container(Type type, Lifetime lifetime)
+        {
+            this.type = type;
+            this.lifetime = lifetime;
+        }
+    }
+
+    public enum Lifetime
+    {
+        Singleton,
+        Instance
+    }
+}
