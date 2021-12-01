27381e136a46490d669d3d01b0511629ed0f396e
Roman Kozko <romanka2001@mail.ru>
2021-12-01 17:09:48 +0300

    add tests

--- DILab/Tests/LibTests.cs
+++ DILab/Tests/LibTests.cs
@@ -1,18 +1,85 @@
 using NUnit.Framework;
 
+using DIContainerLib;
+using DIContainerLib.Configuration;
+using DIContainerLib.Provider;
+using DIContainerLib.Validation;
+
+using Tests.TestClasses;
+
 namespace Tests
 {
+ 
     public class Tests
     {
+        IConfiguration configuration;
+
         [SetUp]
         public void Setup()
         {
+            this.configuration = new DependenciesConfiguration();
         }
 
         [Test]
-        public void Test1()
+        public void TestSimpleDependencyWithSingleton()
         {
-            Assert.Pass();
+
+            configuration.register<IClass,TestClass>(Lifetime.Singleton);
+            IProvider provider = new DependencyProvider(configuration);
+
+            var instance = provider.Resolve(typeof(IClass));
+            var senondInstanse = provider.Resolve(typeof(IClass));
+
+            Assert.AreEqual(instance, senondInstanse);
+         
+           
+        }
+
+        [Test]
+        public void TestSimpleDependencyWithInstanse()
+        {
+
+            configuration.register<IClass, TestClass>(Lifetime.Instance);
+            IProvider provider = new DependencyProvider(configuration);
+
+            var instance = provider.Resolve(typeof(IClass));
+            var senondInstanse = provider.Resolve(typeof(IClass));
+
+            Assert.AreNotEqual(instance, senondInstanse);
+
+        }
+
+        [Test]
+        public void TestOpenGenerics()
+        {
+            configuration.register(typeof(p3<>), typeof(TestClass3<>), Lifetime.Instance);
+            configuration.register<IClass,TestClass>(Lifetime.Instance);
+            IProvider provider = new DependencyProvider(configuration);
+
+            var instance = provider.Resolve<p3<int>>();
+            TestClass3<int> castedClass = (TestClass3<int>)instance;
+           
+            Assert.AreEqual(castedClass.cl.a, 5);
+            
+        }
+
+        [Test]
+        public void TestValidation()
+        {
+            configuration.register(typeof(p3<>), typeof(TestClass3<>), Lifetime.Instance);
+            var validator = new Validator(configuration);
+            
+            var result = validator.isValid();
+            Assert.AreEqual(result, false);
+
+
+            configuration.register(typeof(p3<>), typeof(TestClass3<>), Lifetime.Instance);
+            configuration.register<IClass, TestClass>(Lifetime.Instance);
+            validator = new Validator(configuration);
+
+            result = validator.isValid();
+            Assert.AreEqual(result, true);
+
         }
     }
 }
---
+++ DILab/Tests/TestClasses/TestClasses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TestClasses
+{
+    public interface IClass { int a { get; } };
+
+    public class TestClass : IClass
+    {
+        public int a { get; }
+
+        public TestClass()
+        {
+            this.a = 5;
+        }
+    }
+
+    public interface INewnterface
+    {
+        void doJob();
+    }
+
+    public class TestClass2 : INewnterface
+    {
+
+        public IClass cl;
+
+        public TestClass2(IClass cl)
+        {
+            this.cl = cl;
+        }
+
+        public void doJob()
+        {
+            
+        }
+    }
+
+    public interface p3<T> { };
+
+    public class TestClass3<T> : TestClass2, p3<T>
+    {
+
+        public TestClass3(IClass cl) : base(cl)
+        {
+            
+        }
+    }
+
+ 
+}
